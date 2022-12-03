766aa09e2234a20e9081576ee777cad69a5d72de
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-12-03 17:43:31 +0200

    fix

--- src/main/java/tolik/home/springboot/rickandmortyapp/service/MovieCharacterServiceImpl.java
+++ src/main/java/tolik/home/springboot/rickandmortyapp/service/MovieCharacterServiceImpl.java
@@ -1,12 +1,12 @@
 package tolik.home.springboot.rickandmortyapp.service;
 
+import jakarta.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
